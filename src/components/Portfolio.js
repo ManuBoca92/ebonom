223f57bc1de1b26efac4e54348e152e27b991486
Ebonom Mfam <ebonom.mfam@snow.dog>
2020-05-30 12:08:41 +0200

    removed redundant code

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLink } from "@fortawesome/free-solid-svg-icons";
 
 function Porfolio() {
     return (

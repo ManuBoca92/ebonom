963370653caed42757d0bc19722dafac11048a63
Ebonom Mfam <ebonom.mfam@snow.dog>
2020-05-31 19:26:38 +0200

    changed contact section design

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,29 +3,23 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPhone, faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 class Contact extends Component {
-    render() {
-        return (
-            <div className="contact">
-                <h1 className="title">Contact me</h1>
-                <div className="contact-detail">
-                    <p>
-                        <FontAwesomeIcon
-                            icon={faEnvelope}
-                            className="contact-icon"
-                        />
-                        <span>ebonom.n.mfam@gmail.com</span>
-                    </p>
-                    <p>
-                        <FontAwesomeIcon
-                            icon={faPhone}
-                            className="contact-icon"
-                        />
-                        <span>+48732187071</span>
-                    </p>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="contact">
+        <h1 className="title">contact me</h1>
+        <div className="contact-detail">
+          <p>
+            <FontAwesomeIcon icon={faEnvelope} className="contact-icon" />
+            <span>ebonom.n.mfam@gmail.com</span>
+          </p>
+          <p>
+            <FontAwesomeIcon icon={faPhone} className="contact-icon" />
+            <span>+48732187071</span>
+          </p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Contact;
